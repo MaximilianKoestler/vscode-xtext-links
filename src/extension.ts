e1540468c791f0d02efa7c8a5c8d268aa1c885a7
Maximilian Köstler <maximilian@koestler.hamburg>
2020-11-05 19:19:05 +0100

    Remove sample command

--- src/extension.ts
+++ src/extension.ts
@@ -9,21 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "xtext-links" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "xtext-links.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Xtext Links!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
   vscode.window.registerTerminalLinkProvider({
     provideTerminalLinks: (context, token) => {
       // Detect the first instance of the word "test" if it exists and linkify it
